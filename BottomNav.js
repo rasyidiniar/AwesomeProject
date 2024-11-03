3fbe35820d22454da76c7fd37b6b95d1c8c97281
rasyidiniar <rasyidiniar21@gmail.com>
2024-11-03 19:23:54 +0700

    Bottom Tabs Navigation

--- BottomNav.js
+++ BottomNav.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 function HomeScreen() {
   return (
@@ -24,9 +25,33 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      {/* <Tab.Navigator screenOptions={{ headerStyle: { backgroundColor: '#007AFF' } }}> */}
+      <Tab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#007AFF' },
+          tabBarStyle: { backgroundColor: '#222428' }, 
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="cog" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
